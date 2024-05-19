d260f38a8f8ae92487dafd3e8b5ae2cd104d7456
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2024-05-19 19:53:15 +0200

    Add year and cleanup

--- IPtoDNSLookup/Forms/AboutBox.cs
+++ IPtoDNSLookup/Forms/AboutBox.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LookupsIPsToDNS
@@ -17,7 +12,7 @@ namespace LookupsIPsToDNS
             this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
             this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
+            this.labelCopyright.Text = AssemblyCopyright + @" " + DateTime.Today.Year;
             this.labelCompanyName.Text = AssemblyCompany;
             this.textBoxDescription.Text = AssemblyDescription;
         }
